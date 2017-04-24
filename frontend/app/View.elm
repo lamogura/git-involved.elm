2a8dfc0cf8398774d6457cc276e076cbb04689d8
happysalada <raphael.megzari@gmail.com>
2017-04-25 08:50:10 +0900

    barebone style

--- frontend/app/View.elm
+++ frontend/app/View.elm
@@ -1,7 +1,7 @@
 module View exposing (..)
 
 import Html exposing (Html, button, div, h1, a, p, span, text)
-import Html.Attributes exposing (class, href, id)
+import Html.Attributes exposing (class, style, href, id)
 import Html.Events exposing (onClick)
 import Models exposing (Model)
 import Messages exposing (Msg)
@@ -29,19 +29,20 @@ page model =
 
 
 mainPage : WebData Models.IssueSearchResult -> Html Msg
-mainPage response =
-    Html.body [ class "mdl-demo mdl-color-text--grey-700 mdl-base" ]
+mainPage issuesSearchResult =
+    Html.body [ class "mdl-color-text--grey-700 mdl-base" ]
         [ div [ class "mdl-layout__container has-scrolling-header" ]
-            [ div [ class "mdl-layout mdl-js-layout mdl-layout--fixed-header has-tabs is-upgraded" ]
+            [ div [ class "mdl-layout mdl-layout--fixed-header" ]
                 [ Html.header [ class "mdl-layout__header mdl-layout__header--scroll mdl-color--primary" ]
-                    [ div [ class "mdl-layout--large-screen-only mdl-layout__header-row" ] []
-                    , div [ class "mdl-layout--large-screen-only mdl-layout__header-row" ]
-                        [ Html.h3 [] [ text "Git-Involved" ] ]
+                    [ div [ class "mdl-layout--large-screen-only mdl-layout__header-row" ]
+                        [ Html.h3 [] [ text "Git-Back" ] ]
+                    , div [ class "mdl-layout--large-screen-only mdl-layout__header-row", style [ ( "justify-content", "center" ) ] ]
+                        [ Html.h3 [] [ text "Help out on some OPEN SOURCE projects" ] ]
                     , div [ class "mdl-layout--large-screen-only mdl-layout__header-row" ] []
                     ]
                 , Html.main_ [ class "mdl-layout__content mdl-color--grey-100" ]
-                    [ Html.section [ class "section--center mdl-grid" ]
-                        (maybeIssueSearchResult response)
+                    [ Html.section [ class "section--center mdl-color--white mdl-grid" ]
+                        (maybeIssueSearchResult issuesSearchResult)
                     ]
                 ]
             ]
@@ -67,22 +68,24 @@ maybeIssueSearchResult response =
 issueDiv : Models.Issue -> Html Msg
 issueDiv issue =
     div [ class "git-issue-card mdl-cell--12-col mdl-shadow--2dp" ]
-        [ div [ class "git-issue mdl-cell mdl-cell--9-col" ]
+        [ div [ class "git-issue mdl-cell--9-col" ]
             [ div [ class "mdl-card__supporting-text" ]
-                [ Html.h4 [ class "issue-title" ] [ text ("Title: " ++ issue.title) ]
-                , div [ class "issue-body" ] [ text ("Body: " ++ issue.body) ]
-                ]
-            , div [ class "mdl-card__actions" ]
-                [ div [ class "issue-labels" ] (List.map labelDiv issue.labels)
-                , div [ class "issue-comments mdl-button" ] [ text ("Comments: " ++ toString issue.commentCount) ]
-                ]
-            ]
-        , div [ class "git-repo mdl-cell mdl-cell--3-col" ]
-            [ div [ class "mdl-card__supporting-text" ]
-                [ Html.h5 [ class "repo-title" ] [ text ("Repo: " ++ (repoNameFromUrl issue.repository_url)) ]
+                [ Html.h4 [ class "issue-title" ] [ text issue.title ]
+                , div [ class "issue-body" ]
+                    [ if String.isEmpty issue.body then
+                        text "No description"
+                      else
+                        text issue.body
+                    ]
                 ]
             , div [ class "mdl-card__actions" ]
                 [ div [ class "issue-comments mdl-button" ] [ text ("Comments: " ++ toString issue.commentCount) ]
+                , div [ class "issue-labels" ] (List.map labelDiv issue.labels)
+                ]
+            ]
+        , div [ class "git-repo mdl-cell mdl-cell--3-col" ]
+            [ div [ class "mdl-card__supporting-text", id "git-repo" ]
+                [ Html.h5 [ class "repo-title" ] [ text ("Repo: " ++ (repoNameFromUrl issue.repository_url)) ]
                 ]
             ]
         ]
